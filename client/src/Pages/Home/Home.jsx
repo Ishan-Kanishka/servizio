f7dc1fccbbc31415f63f3bc3a2a59787fea5d09b
Thamidu-Nadun <nadunrz101@gmail.com>
2025-09-30 14:39:33 +0530

    Test

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
 import Hero from './Hero/Hero';
+import Test from './Test';
 
 const Home = () => {
   return (
     <Fragment>
       <Hero />
+      <Test />
     </Fragment>
   );
 };
