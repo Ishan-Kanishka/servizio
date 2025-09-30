eccc134bdb5aed046afa037d40944f60115dfec3
Thamidu-Nadun <nadunrz101@gmail.com>
2025-09-30 13:32:39 +0530

    event

--- server/src/main/java/com/servizo/servizo/controller/EventController.java
+++ server/src/main/java/com/servizo/servizo/controller/EventController.java
@@ -1,12 +1,15 @@
 package com.servizo.servizo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/v1/event/")
+@RequestMapping("/api/v1/events")
 public class EventController {
 
 }
