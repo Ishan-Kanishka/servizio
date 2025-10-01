501b371bce04aab64162dec10e19f5d6b8ab7790
Thamidu-Nadun <nadunrz101@gmail.com>
2025-10-01 11:08:59 +0530

    Add Login page and implement form handling; create Customers, Events, Ingredients, Menu, Orders, and Roles components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,31 @@ import {Suspense} from 'react';
 import DashboardLayout from './layout/DashboardLayout';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import NotFound from './Pages/NotFound/NotFound';
-import Menu from './Pages/Dashboard/Menu/Menu';
-import Orders from './Pages/Dashboard/Orders/Orders';
-import Ingredients from './Pages/Dashboard/Ingredients/Ingredients';
-import Event from './Pages/Dashboard/Events/Events';
-import Customers from './Pages/Dashboard/Customers/Customers';
-import Staff from './Pages/Dashboard/Staff/Staff';
-import Roles from './Pages/Dashboard/Roles/Roles';
+import Menu from './Pages/Dashboard/Pages/Menu/Menu';
+import Orders from './Pages/Dashboard/Pages/Orders/Orders';
+import Ingredients from './Pages/Dashboard/Pages/Ingredients/Ingredients';
+import Event from './Pages/Dashboard/Pages/Events/Events';
+import Customers from './Pages/Dashboard/Pages/Customers/Customers';
+import Staff from './Pages/Dashboard/Pages/Staff/Staff';
+import Roles from './Pages/Dashboard/Pages/Roles/Roles';
+import Login from './Pages/Login/Login';
 
 const App = () => {
   return (
     <Suspense fallback={'Loading ...!'}>
       <Routes>
+        {/* Main */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+
+          {/* Login */}
+          <Route path="/login" element={<Login />} />
+
+          {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Route>
+
+        {/* Dashboard */}
         <Route path="/admin" element={<DashboardLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="orders" element={<Orders />} />
